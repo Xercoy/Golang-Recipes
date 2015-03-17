55fc2b7a4b319b1e1d8ef885f7a0a57ac26f9835
Xercoy <corey@prak.me>
2015-03-16 23:22:57 -0400

    server.go: can now serve resources as text

--- server.go
+++ server.go
@@ -3,10 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"io/ioutil"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, world!")
+        var fileName string = string(r.URL.Path[1:])
+
+        file, err := ioutil.ReadFile(fileName)		
+	if (err != nil) {
+		fmt.Fprintf(w, "Error 404: resource not found!")
+	}
+
+	fmt.Fprintf(w, string(file))
 }
 
 func main() {
