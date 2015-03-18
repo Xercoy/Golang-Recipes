d6c9cd63c860511e6b3387b997735c5327c485ca
Xercoy <corey@prak.me>
2015-03-18 14:38:35 -0400

    server.go: added template functionality, can serve basic page.
    basic_template.go: basic template
    
    Helpful Resources:
    
    Generate plain text to HTML:
    Could provide <!DOCTYPE html> or provide header as text/html
    http://stackoverflow.com/questions/25066071/golang-html-output-is-being-interpreted-as-plain-text-instead-of-being-received
    
    Type conversion:
    https://groups.google.com/forum/#!topic/golang-nuts/84GCvDBhpbg
    
    Saving template output to a buffer:
    Initialize a buffer, and when there is a need for io.Writer, provide
    the buffer address.
    Source: https://groups.google.com/forum/#!topic/golang-nuts/DLJvNiwTcqE
    
    Using template.Parse VS template.ParseFiles:
    When using ParseFiles or ParseGlob, use template.ExecuteTemplate
    http://stackoverflow.com/questions/11805356/text-template-issue-parse-vs-parsefiles
    
    Preserving Whitespace:
    Add the style white-space:pre-wrap;
    http://blog.wolffmyren.com/2009/07/24/dev-tip-preserve-whitespace-in-html-without-using/

--- server.go
+++ server.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 )
+
+type Recipe struct {
+	Title, Content string
+}
+
+func prepareResponse(title string, content string) string {
+	var newRecipe = Recipe{title, content}
+
+	var t = template.Must(template.New("recipe").ParseFiles("basic_template.txt"))
+
+	var buf bytes.Buffer
+
+	t.ExecuteTemplate(&buf, "basic_template.txt", newRecipe)
+
+	return buf.String()
+}
 
 func getFileContent(fileName string) string {
 	fileContent, err := ioutil.ReadFile(fileName)
@@ -16,11 +34,15 @@ func getFileContent(fileName string) string {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	//Retrieve file content.
 	var fileName string = string(r.URL.Path[1:])
 	fileContent := getFileContent(fileName)
 
-	//Write response
-	fmt.Fprintf(w, string(fileContent))
+	//Get response ready
+	response := prepareResponse(fileName, fileContent)
+
+	//Write response.
+	fmt.Fprintf(w, response)
 }
 
 func main() {
