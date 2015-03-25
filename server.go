5dd3b461aebc4f6aa60fee593ced4571897335a4
Xercoy <corey@prak.me>
2015-03-25 01:28:02 -0400

    server.go: now serves index, recipe, and error pages correctly.
    - Need to work on front end so more changes are needed.

--- server.go
+++ server.go
@@ -1,78 +1,91 @@
 package main
 
 import (
-//	"bytes"
+	"bytes"
 	"fmt"
-//	"html/template"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 )
 
-type Recipe struct {
+type RecipePage struct {
 	Title, Content string
 }
-/*
-func prepareResponse(title string, content string) string {
-	var newRecipe = Recipe{title, content}
 
-	var t = template.Must(template.New("recipe").ParseFiles("basic_template.txt"))
+func getFileContent(fileName string, directive string) ([]byte, error) {
+	var err error
+	var fileContent []byte
+
+	switch directive {
+	case "recipes":
+		fileContent, err = ioutil.ReadFile("recipes/" + fileName)
+
+	case "root":
+		fileContent, err = ioutil.ReadFile(fileName)
+	}
+
+	return fileContent, err
+}
+
+func parseTemplate(fileName string, fileContent string) []byte {
+	var rPage = RecipePage{fileName, fileContent}
+
+	var t = template.Must(template.New("page").ParseFiles("basic_template.txt"))
 
 	var buf bytes.Buffer
 
-	t.ExecuteTemplate(&buf, "basic_template.txt", newRecipe)
+	t.ExecuteTemplate(&buf, "basic_template.txt", rPage)
 
-	return buf.String()
-}*/
-
-func getFileContent(fileName string)  string {
-	fileContent, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		panic(err)
-	}
-
-	return string(fileContent)
+	return buf.Bytes()
 }
-/*
-func handler(w http.ResponseWriter, r *http.Request) {
-	//Retrieve file content.
-	var fileName string = string(r.URL.Path[1:])
-	fileContent := getFileContent(fileName)
 
-	//Get response ready
-	response := prepareResponse(fileName, fileContent)
-
-	//Write response.
-	fmt.Fprintf(w, response)
-}*/
-func prepareResponse(directive string, path string) []byte {
+func prepareResponse(directive string, fileName string) ([]byte, error) {
 	var response []byte
-	
+	var err error
+	var fileContent []byte
+
 	switch directive {
 
 	case "root":
-		response = []byte(getFileContent("index_template.txt"))
+		response, err = getFileContent("index_template.txt", "root")
+		
+	case "recipes":
+
+		fileContent, err = getFileContent(fileName, "recipes")
+
+        response = []byte(parseTemplate(fileName, string(fileContent)))
 	}
-	
-	return response
+
+	return response, err
 }
 
-func handler (w http.ResponseWriter, r *http.Request) {
+func handler(w http.ResponseWriter, r *http.Request) {
 
-	var response []byte
+	var response []byte = []byte("Resource Not Found!")
+	
+	var err error
 
-	if (r.URL.Path[1:] == "") {
-		response =  prepareResponse("root", "")
-	} else if ( r.URL.Path[2:8] == "recipes" ) {
-		response = prepareResponse("recipes", r.URL.Path[9:])
+	/* Checks if path is at least /recipes, assumes there is more to the URL
+	   since it is ensuring that the length is greater than /recipes (8). */
+	if ((len(r.URL.Path) > 8) && (r.URL.Path[1:8] == "recipes") ){
+		response, err = prepareResponse("recipes", r.URL.Path[8:])
+
+	// Checks if path is root or exactly /recipes, loads homepage.
+	} else if (r.URL.Path == "/") || (r.URL.Path[1:8] == "recipes")   {
+		response, err = prepareResponse("root", "")
+	}
+
+	/* If there is any kind of error preparing the response, 
+           display accordingly. */
+	if (err != nil) {
+		response = []byte("Error, resource not found!")
 	}
 
 	//Write response.
-	fmt.Fprintf(w, string(response))
+	fmt.Fprintf(w, "%s", string(response))
 }
 
 func main() {
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":8080", nil)
 }
-
-
