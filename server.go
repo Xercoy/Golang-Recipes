2f72fa251e7413bd4d9eb25af86f0dc6c2d5c089
Xercoy <corey@prak.me>
2015-03-28 04:48:27 -0400

    Serves pages but there isn't any generated content.

--- server.go
+++ server.go
@@ -6,10 +6,69 @@ import (
 	"html/template"
 	"io/ioutil"
 	"net/http"
+	"os"
+	"bufio"
 )
 
 type RecipePage struct {
-	Title, Content string
+	Title, Content       string
+	RecipeName []string
+	FileName []string
+}
+
+func getSliceOfFileNames(dir string) []string {
+	var sliceOfFileInfos []os.FileInfo
+	var err error
+	var fileNames []string
+
+	sliceOfFileInfos, err = ioutil.ReadDir(dir)
+	if err != nil {
+		panic(err)
+	}
+
+	for _, fileInfo := range sliceOfFileInfos {
+		fileNames = append(fileNames, fileInfo.Name())
+	}
+
+	return fileNames
+}
+
+func getSliceOfRecipeNames(dir string) []string {
+	fileNames := getSliceOfFileNames(dir)
+
+	var rNames []string
+
+	for _, fname := range fileNames {
+
+		filePointer, err := os.Open(dir + fname)
+		if err != nil {
+			panic(err)
+		}
+
+		scanner := bufio.NewScanner(filePointer)
+
+		if scanner.Scan() {
+			rNames = append(rNames, (scanner.Text())[2:])
+		} else {
+			panic("EMPTY SCAN")
+		}
+
+		filePointer.Close()
+	}
+
+	return rNames
+}
+
+func (self RecipePage) GenerateLinks() []string {
+	var linkSlice []string
+
+	for index, rName := range self.RecipeName {
+		link := "<p><a href=\"" + self.FileName[index] + "\">" + rName + "</a></p>"
+
+		linkSlice = append(linkSlice, link)
+	}
+
+	return linkSlice
 }
 
 func getFileContent(fileName string, directive string) ([]byte, error) {
@@ -27,14 +86,36 @@ func getFileContent(fileName string, directive string) ([]byte, error) {
 	return fileContent, err
 }
 
-func parseTemplate(fileName string, fileContent string) []byte {
-	var rPage = RecipePage{fileName, fileContent}
+func parseTemplate(fileName string, fileContent string, directive string) []byte { /*
+		var rPage = RecipePage{fileName, fileContent}
 
-	var t = template.Must(template.New("page").ParseFiles("basic_template.txt"))
+		var t = template.Must(template.New("page").ParseFiles("basic_template.txt"))
 
+		var buf bytes.Buffer
+
+		t.ExecuteTemplate(&buf, "basic_template.txt", rPage)
+	*/
+
+	var rPage RecipePage
+	var t *template.Template
 	var buf bytes.Buffer
+	var templateFileName []byte
 
-	t.ExecuteTemplate(&buf, "basic_template.txt", rPage)
+	switch directive {
+	case "root":
+		fileNameSlice := getSliceOfFileNames("./recipes/")
+		recipeNameSlice := getSliceOfRecipeNames("./recipes/")
+
+		rPage = RecipePage{fileName, fileContent, fileNameSlice, recipeNameSlice}
+		templateFileName = []byte("index_template.txt")
+	case "recipe":
+		rPage = RecipePage{fileName, fileContent, []string{""}, []string{""}}
+		templateFileName = []byte("basic_template.txt")
+	}
+
+	t = template.Must(template.New("page").ParseFiles(string(templateFileName)))
+
+	t.ExecuteTemplate(&buf, string(templateFileName), rPage)
 
 	return buf.Bytes()
 }
@@ -48,12 +129,14 @@ func prepareResponse(directive string, fileName string) ([]byte, error) {
 
 	case "root":
 		response, err = getFileContent("index_template.txt", "root")
+//		fileContent, err = getFileContent(fileName, "root")
 
+		response = []byte(parseTemplate(fileName, string(fileContent), "root"))
 	case "recipes":
 
 		fileContent, err = getFileContent(fileName, "recipes")
 
-		response = []byte(parseTemplate(fileName, string(fileContent)))
+		response = []byte(parseTemplate(fileName, string(fileContent), "recipes"))
 	}
 
 	return response, err
@@ -77,7 +160,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	/* If there is any kind of error preparing the response,
 	   handle it by displaying the index page. */
 	if err != nil {
-		response = []byte("ERROR, you've reached an invalid page.")
+	//	response = []byte("ERROR, you've reached an invalid page." + string(err)) 
+		panic(err)
 	}
 
 	//Write response.
@@ -86,5 +170,5 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":3000", nil)
 }
