7903d6bfcaab17182cd667779a86139b48027065
Xercoy <corey@prak.me>
2015-03-27 20:47:33 -0400

    recipes/os.go: Added Open example for the os package

---
+++ recipes/os.go
@@ -0,0 +1,35 @@
+//os
+package main
+
+import (
+	"fmt"
+	"bufio"
+	"os"
+)
+
+func main() {
+/* Open Example Begin */
+	/* This example opens a file, reads it one line at a time,
+           and prints the line to stdout directly after it is read. */
+
+	var filePointer *os.File
+	var err error
+	var scanner *bufio.Scanner
+
+	// CHANGE AS NEEDED
+	var fileName string = "ioutil.go"
+
+	filePointer, err = os.Open(fileName)
+	if (err != nil) {
+		panic(err)
+	}
+
+	scanner = bufio.NewScanner(filePointer)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	filePointer.Close()
+/* Open Example End */
+}
