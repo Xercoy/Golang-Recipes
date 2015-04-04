cc7a1d48fba22c514a7cd4733c1a13b1e27e67fe
Xercoy <corey@prak.me>
2015-04-03 21:03:29 -0400

    server.go: gofmt

--- server.go
+++ server.go
@@ -1,23 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	"html/template"
 	"io/ioutil"
 	"net/http"
 	"os"
-	"bufio"
 )
 
-/* Considered creating diff structs for each directive but that means more 
-   coding and more things to keep track of... Who knows, maybe there will be a 
-   need for a page to keep track of all of this info. Maybe the master struct 
+/* Considered creating diff structs for each directive but that means more
+   coding and more things to keep track of... Who knows, maybe there will be a
+   need for a page to keep track of all of this info. Maybe the master struct
    should be an aggregation of all of the different structs? */
 type RecipePage struct {
-	Title, Content       string
-	RecipeName []string
-	FileName []string
+	Title, Content string
+	RecipeName     []string
+	FileName       []string
 }
 
 func getSliceOfFileNames(dir string) []string {
@@ -63,21 +63,20 @@ func getSliceOfRecipeNames(dir string) []string {
 	return rNames
 }
 
-
 // Appending a string: http://stackoverflow.com/questions/1760757/how-to-efficiently-concatenate-strings-in-go
 func (self RecipePage) GenerateLinks() template.HTML {
 	var buffer bytes.Buffer
 
 	for index, rName := range self.RecipeName {
 		link := "<p><a href=\"/recipes/" + self.FileName[index] + "\">" + rName + "</a></p>"
-		
+
 		buffer.WriteString(link)
 	}
 
 	return template.HTML(buffer.String())
 }
 
-/* Return the contents of a file as a slice of bytes. The directive tells the 
+/* Return the contents of a file as a slice of bytes. The directive tells the
    function where to look for the files. For instance, all recipes reside in the
    recipes folder. Need to place templates in their own folder... */
 func getFileContent(fileName string, directive string) ([]byte, error) {
@@ -88,17 +87,17 @@ func getFileContent(fileName string, directive string) ([]byte, error) {
 	case "recipes":
 		fileContent, err = ioutil.ReadFile("recipes/" + fileName)
 
-	case "root": 
+	case "root":
 		fileContent, err = ioutil.ReadFile(fileName)
 	}
 
 	return fileContent, err
 }
 
-/* parseTemplate is called by responseHandler. This function receives the 
-   information needed such as file names so that it could run said data 
-   against the template and return the result. */ 
-func parseTemplate(fileName string, fileContent string, directive string) []byte { 
+/* parseTemplate is called by responseHandler. This function receives the
+   information needed such as file names so that it could run said data
+   against the template and return the result. */
+func parseTemplate(fileName string, fileContent string, directive string) []byte {
 	var rPage RecipePage
 	var t *template.Template
 	var buf bytes.Buffer
@@ -110,13 +109,13 @@ func parseTemplate(fileName string, fileContent string, directive string) []byte
 		fileNameSlice := getSliceOfFileNames("./recipes/")
 		recipeNameSlice := getSliceOfRecipeNames("./recipes/")
 
-		/* Fill out the data structure accordingly. This template 
-                   doesn't use the first two fields bc they're for recipes */
+		/* Fill out the data structure accordingly. This template
+		   doesn't use the first two fields bc they're for recipes */
 		rPage = RecipePage{"", "", recipeNameSlice, fileNameSlice}
 
 	case "recipes":
-		/* Recipe template doesn't need every recipes' title/file name 
-                   so leave both blank */
+		/* Recipe template doesn't need every recipes' title/file name
+		   so leave both blank */
 
 		// Template for the recipes page.
 		rPage = RecipePage{fileName, fileContent, []string{""}, []string{""}}
@@ -131,7 +130,6 @@ func parseTemplate(fileName string, fileContent string, directive string) []byte
 	return buf.Bytes()
 }
 
-
 /* Based on the directive given, responseHandler calls parseTemplate so that
    the final, complete response */
 func responseHandler(directive string, fileName string) ([]byte, error) {
@@ -145,7 +143,7 @@ func responseHandler(directive string, fileName string) ([]byte, error) {
 		response = []byte(parseTemplate(fileName, string(""), "root"))
 
 	case "recipes":
-		// Get content of the recipe. 
+		// Get content of the recipe.
 		fileContent, err = getFileContent(fileName, "recipes")
 
 		// Fill the recipes template with code.
@@ -155,12 +153,12 @@ func responseHandler(directive string, fileName string) ([]byte, error) {
 	return response, err
 }
 
-/* The most important thing pathHandler does is determine whether the resource 
+/* The most important thing pathHandler does is determine whether the resource
    is valid. Depending on whether the desired resource is the root of the page
-   or a recipe, pathHandler will call responseHandler to prepare a response 
+   or a recipe, pathHandler will call responseHandler to prepare a response
    to the request received. A directive is included to responseHandler as the
-   first argument. Since the responses are prepared differently, this tells the 
-   function whether it is receiving a path for a recipe or for the root page. 
+   first argument. Since the responses are prepared differently, this tells the
+   function whether it is receiving a path for a recipe or for the root page.
    There is no functionality to display an invalid resource at the moment. */
 func pathHandler(w http.ResponseWriter, r *http.Request) {
 	var response []byte = []byte("Resource Not Found!")
@@ -175,7 +173,7 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 
 		response, err = responseHandler("recipes", r.URL.Path[9:])
 
-	} else if ((path == "/") || (path == "/recipes") || (path == "/recipes/")){
+	} else if (path == "/") || (path == "/recipes") || (path == "/recipes/") {
 
 		response, err = responseHandler("root", "")
 
@@ -191,8 +189,7 @@ func pathHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "%s", string(response))
 }
 
-
-// The pathHandler function is responsible to handling all requests. 
+// The pathHandler function is responsible to handling all requests.
 func main() {
 	http.HandleFunc("/", pathHandler)
 	http.ListenAndServe(":8080", nil)
